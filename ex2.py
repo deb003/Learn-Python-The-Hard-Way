fad985c31cc23109700de0f59f7fec175ab4552d
Tapasweni Pathak <tapaswenipathak@gmail.com>
2015-02-18 23:01:23 +0530

    first 3

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignore by python.
+
+print "I could have code like this." #and the comment after is ignored
+
+# You can also use a comment to "disable" a comment out of piece of code:
+# print "This won't run."
+
+print "This will run."
+
+# -*- coding: utf-8 -*-
