22566db503b0f68a5364ff4d51e0b5c49443778c
deb003 <dkg1292@gmail.com>
2016-01-06 02:34:47 +0530

    Variable Declarations can be avoided
    Variable Declarations can be avoided

--- ex11.py
+++ ex11.py
@@ -1,8 +1,2 @@
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
-
-print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
+print "So you're %d old, %d tall and %d heavy." %(raw_input("How old are you?"), 
+      raw_input("How tall are you?"), raw_input("How much do you weigh?"))
