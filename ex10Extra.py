6e65442c263b933551e0023ad6f4ffd6f5b23d31
deb003 <dkg1292@gmail.com>
2016-01-06 02:13:19 +0530

    Updating to stop infinite loop
    Tried to set a count which will stop the loop being an infinite one. Please comment back if you have any queries

--- ex10Extra.py
+++ ex10Extra.py
@@ -1,5 +1,8 @@
+count = 0
 while True:
-    for i in ["/","-","|","\\","|"]:
-        print "%s\r" % i,
-
+    for i in ["/","-","|","\\","|",5]:
+        print "%s" % i,
+    count += 1
+    if count > 8:
+        break
 #If you want to understand it more, remove the \r in print statement.
